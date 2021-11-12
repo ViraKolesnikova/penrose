d76e7bfed461c8435df70045811ee0749976edda
ViraKolesnikova <82116461+ViraKolesnikova@users.noreply.github.com>
2021-11-12 17:00:15 +0200

    slider

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
 import './sass/main.scss';
+import template from './template/dataMarkUp.hbs';
+import refs from './js/refs.js'
 
-const subMenuBtn = document.querySelector('#sub-menu-btn');
-const subMenuList = document.querySelector('.sub-menu-list');
-const bottomSvg = document.querySelector('.bottom');
-const topSvg = document.querySelector('.top');
-const burgerBtn = document.querySelector('.burger-btn');
-const closeBtn = document.querySelector('.close-menu-btn');
-const mobileNavRef = document.querySelector('.navigation-mobile');
+const{subMenuBtn, subMenuList, bottomSvg, topSvg, burgerBtn , closeBtn,navRef, mobileNavRef, sliderContent, prevBtn, nextBtn, dotsContainer}=refs
+
+
 
 subMenuBtn.addEventListener('click', onSubMenuClick);
+document.addEventListener('click', onDocumentCloseSubmenu);
 
 burgerBtn.addEventListener('click', () => {
   mobileNavRef.classList.toggle('is-open')
@@ -19,19 +18,58 @@ closeBtn.addEventListener('click', () => {
 })
 
 
-
-
-
 function onSubMenuClick() {
-  if (subMenuList.classList.contains('is-hidden')) {
-    subMenuList.classList.remove('is-hidden');
-    bottomSvg.classList.add('is-hidden');
-    topSvg.classList.remove('is-hidden');
-  } else {
-    subMenuList.classList.add('is-hidden');
-    topSvg.classList.add('is-hidden');
-    bottomSvg.classList.remove('is-hidden');
-  }
+  subMenuList.classList.contains('is-hidden') ? openSubmenu() : closeSubMenu();
+  
+}
+
+function openSubmenu() {
+  subMenuList.classList.remove('is-hidden');
+  bottomSvg.classList.add('is-hidden');
+  topSvg.classList.remove('is-hidden');  
+}
+
+function closeSubMenu() {
+  subMenuList.classList.add('is-hidden');
+  topSvg.classList.add('is-hidden');
+  bottomSvg.classList.remove('is-hidden');  
+}
+
+fetchData()
+
+async function fetchData() {
+  const response = await fetch('https://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline');
+  const result = response.json();
+  const filteredData = await filtrDataByPrice(result);
+  renderMarkUp(filteredData);
+  appendDots(filteredData);
 }
 
 
+async function filtrDataByPrice(data) {
+  const dataArr = await data;
+  return dataArr.filter(item => item.price < 5);
+}
+
+async function renderMarkUp(data) {
+  const markUp = await template(data);
+  sliderContent.innerHTML = markUp;
+}
+
+function onDocumentCloseSubmenu(event) {
+  if (!subMenuList.classList.contains('is-hidden') && event.target!==subMenuBtn) {
+    closeSubMenu();
+  } 
+}
+
+async function appendDots(data) {
+  const dotsAmount = await data.length;
+  for (let i = 1; i < dotsAmount; i += 1){
+    const item = document.createElement('li');
+    const a = document.createElement('a');
+    const span = document.querySelector('span');
+    
+    '<li><a href="#"><span class="dot"></span></a></li>';
+  }
+  dotsContainer.append
+}  
---
+++ src/js/refs.js
@@ -0,0 +1,14 @@
+export default {
+ subMenuBtn: document.querySelector('#sub-menu-btn'),
+ subMenuList : document.querySelector('.sub-menu-list'),
+ bottomSvg :document.querySelector('.bottom'),
+ topSvg : document.querySelector('.top'),
+ burgerBtn : document.querySelector('.burger-btn'),
+  closeBtn: document.querySelector('.close-menu-btn'),
+ navRef: document.querySelector('.navigation'),
+ mobileNavRef : document.querySelector('.navigation-mobile'),
+ sliderContent : document.querySelector('.slider-container'),
+ prevBtn : document.querySelector('#prev-btn'),
+  nextBtn: document.querySelector('#next-btn'),
+ dotsContainer: document.querySelector('.dots-list'),
+}

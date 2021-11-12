5ce166bbd72b6733371805e2801535d27da45fcf
ViraKolesnikova <82116461+ViraKolesnikova@users.noreply.github.com>
2021-11-12 02:02:31 +0200

    mobile header

--- src/index.js
+++ src/index.js
@@ -1,58 +1,37 @@
 import './sass/main.scss';
 
-
 const subMenuBtn = document.querySelector('#sub-menu-btn');
 const subMenuList = document.querySelector('.sub-menu-list');
 const bottomSvg = document.querySelector('.bottom');
 const topSvg = document.querySelector('.top');
 const burgerBtn = document.querySelector('.burger-btn');
 const closeBtn = document.querySelector('.close-menu-btn');
-const navRef = document.querySelector('.navigation');
-
+const mobileNavRef = document.querySelector('.navigation-mobile');
 
 subMenuBtn.addEventListener('click', onSubMenuClick);
 
-function changeMenuOnBurger() {
-  if (window.innerWidth < 1366) {
-    burgerBtn.addEventListener('click', onOpenBurger);
-    closeBtn.addEventListener('click', onBurgerClose);
-    burgerBtn.style.display = 'block';
-    closeBtn.style.display = 'block';
-   }
-}
-function changeMenuOnBurger()
+burgerBtn.addEventListener('click', () => {
+  mobileNavRef.classList.toggle('is-open')
+});
+
+closeBtn.addEventListener('click', () => {
+  mobileNavRef.classList.toggle('is-open')
+})
+
+
+
 
 
 function onSubMenuClick() {
   if (subMenuList.classList.contains('is-hidden')) {
     subMenuList.classList.remove('is-hidden');
     bottomSvg.classList.add('is-hidden');
-    topSvg.classList.remove('is-hidden')
+    topSvg.classList.remove('is-hidden');
   } else {
     subMenuList.classList.add('is-hidden');
     topSvg.classList.add('is-hidden');
-    bottomSvg.classList.remove('is-hidden')
+    bottomSvg.classList.remove('is-hidden');
   }
-  
-}
-
-
-function onOpenBurger(event) {
-  
-    navRef.classList.remove('is-hidden');
-    burgerBtn.style.display = 'none';
-    closeBtn.style.display = 'block';
-    closeBtn.addEventListener('click', onBurgerClose);
-  
-}
-
-function onBurgerClose(event) {
-  
-    navRef.classList.add('is-hidden');
-    burgerBtn.style.display = 'block';
-    closeBtn.style.display = 'none';
-    closeBtn.removeEventListener('click', onBurgerClose);
-  
 }
 
 

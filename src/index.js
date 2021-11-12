53872877b19667143cefc6f10a5f7a4856ddf6d1
ViraKolesnikova <82116461+ViraKolesnikova@users.noreply.github.com>
2021-11-13 00:19:40 +0200

    mobile-menu and slider CSS

--- src/index.js
+++ src/index.js
@@ -2,40 +2,89 @@ import './sass/main.scss';
 import template from './template/dataMarkUp.hbs';
 import refs from './js/refs.js'
 
-const{subMenuBtn, subMenuList, bottomSvg, topSvg, burgerBtn , closeBtn,navRef, mobileNavRef, sliderContent, prevBtn, nextBtn, dotsContainer}=refs
+const {
+  subMenuBtn,
+  subMenuMobileBtn,
+  subMenuList,
+  subMenuMobileList,
+  bottomSvg,
+  topSvg,
+  mobileBottomSvg,
+  mobileTopSvg,
+  burgerBtn,
+  closeBtn,
+  mobileNavRef,
+  sliderContent,
+  prevBtn,
+  nextBtn,
+  dotsContainer,
+} = refs
 
 
 
 subMenuBtn.addEventListener('click', onSubMenuClick);
 document.addEventListener('click', onDocumentCloseSubmenu);
-
-burgerBtn.addEventListener('click', () => {
-  mobileNavRef.classList.toggle('is-open')
-});
-
-closeBtn.addEventListener('click', () => {
-  mobileNavRef.classList.toggle('is-open')
-})
+burgerBtn.addEventListener('click', onMobileMenuOpen);
+closeBtn.addEventListener('click', onMobileMenuClose);
+mobileNavRef.addEventListener('click', onCloseMenuByLinkClick)
 
 
-function onSubMenuClick() {
-  subMenuList.classList.contains('is-hidden') ? openSubmenu() : closeSubMenu();
+fetchData();
+
+  
+function onMobileMenuOpen() {
+  mobileNavRef.classList.toggle('is-open');
+  activateMobileSubMenu();
+}
+
+function onMobileMenuClose() {
+  mobileNavRef.classList.toggle('is-open');
+  activateMobileSubMenu();
+}
+
+
+function activateMobileSubMenu() {
+  if (mobileNavRef.classList.contains('is-open')) {
+    subMenuMobileBtn.addEventListener('click', onMobileSubMenuClick);
+  } else{subMenuMobileBtn.removeEventListener('click', onMobileSubMenuClick)}
   
 }
 
-function openSubmenu() {
-  subMenuList.classList.remove('is-hidden');
-  bottomSvg.classList.add('is-hidden');
-  topSvg.classList.remove('is-hidden');  
+
+function onSubMenuClick() {
+  subMenuList.classList.contains('is-hidden') ? openSubmenu(subMenuList,bottomSvg,topSvg) : closeSubMenu(subMenuList,bottomSvg,topSvg);  
 }
 
-function closeSubMenu() {
-  subMenuList.classList.add('is-hidden');
-  topSvg.classList.add('is-hidden');
-  bottomSvg.classList.remove('is-hidden');  
+function onMobileSubMenuClick(event) {
+  subMenuMobileList.classList.contains('is-hidden') ? openSubmenu(subMenuMobileList,mobileBottomSvg, mobileTopSvg) : closeSubMenu(subMenuMobileList,mobileBottomSvg, mobileTopSvg);  
 }
 
-fetchData()
+function openSubmenu(list, bottom, top) {
+  list.classList.remove('is-hidden');
+  bottom.classList.add('is-hidden');
+  top.classList.remove('is-hidden');  
+}
+
+function closeSubMenu(list, bottom, top) {
+  list.classList.add('is-hidden');
+  top.classList.add('is-hidden');
+  bottom.classList.remove('is-hidden');  
+}
+
+function onDocumentCloseSubmenu(event) {
+  if (!subMenuList.classList.contains('is-hidden') && event.target!==subMenuBtn) {
+    closeSubMenu(subMenuList,bottomSvg,topSvg);
+  } else if (!subMenuMobileList.classList.contains('is-hidden') && event.target!==subMenuMobileBtn) {
+    closeSubMenu(subMenuMobileList, mobileBottomSvg, mobileTopSvg);
+    onMobileMenuClose();
+  }
+}
+
+function onCloseMenuByLinkClick(event) {
+  if (event.target.nodeName==='A') {
+    onMobileMenuClose();
+  }
+}
 
 async function fetchData() {
   const response = await fetch('https://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline');
@@ -56,20 +105,19 @@ async function renderMarkUp(data) {
   sliderContent.innerHTML = markUp;
 }
 
-function onDocumentCloseSubmenu(event) {
-  if (!subMenuList.classList.contains('is-hidden') && event.target!==subMenuBtn) {
-    closeSubMenu();
-  } 
-}
+
 
 async function appendDots(data) {
   const dotsAmount = await data.length;
-  for (let i = 1; i < dotsAmount; i += 1){
-    const item = document.createElement('li');
+  for (let i = 0; i < dotsAmount; i += 1){
+    const li = document.createElement('li');
     const a = document.createElement('a');
-    const span = document.querySelector('span');
-    
-    '<li><a href="#"><span class="dot"></span></a></li>';
+    const span = document.createElement('span');
+    a.setAttribute('href', '#');
+    span.classList.add('dot');
+    li.classList.add('dots-item');
+    a.appendChild(span);
+    li.appendChild(a);    
+    dotsContainer.append(li);
   }
-  dotsContainer.append
 }  
